e2ddd041befbf3a9086416a436ca4aaeeaab6465
Debabrata Batabyal <dbatabyal2018@gmail.com>
2023-05-03 21:20:14 +0530

    style : mobile sidebar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, { lazy, Suspense } from "react";
 import "./Navbar.css";
 import ham from "../../assets/Navbar/ham.svg";
-import search from "../../assets/Navbar/search.svg";
-import cart from "../../assets/Navbar/cart.svg";
+import close from "../../assets/Navbar/close.svg";
 import parcello from "../../assets/Navbar/parcello.png";
 import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import Login from "../../Components/login/Login";
 
@@ -119,67 +118,69 @@ const Navbar = () => {
           onClick={() => setIsCollapsed(!isCollapsed)}
           className="hidden md:block"
         >
-          <img src={ham}></img>
+          {isCollapsed ? <img src={ham}></img> : <img src={close}></img>}
         </button>
       </div>
-      
-      {!isCollapsed && (
-        
-        <div className="hidden md:block">
-          <div className="flex flex-col items-center justify-center gap-4   w-full absolute bg-white z-40 text-slate-500 p-5  ">
-            <div className="flex gap-4 border-b-[1px] border-slate-300 w-full items-center justify-center p-2">
-              <NavLink
-                to="/signin"
-                onClick={() => {
-                  setOpenLogin(true);
-                }}
-              >
-                SignIn
-              </NavLink>
-              /
-              <NavLink
-                to="/signup"
-                onClick={() => {
-                  setOpenLogin(true);
-                }}
-              >
-                SignUp
-              </NavLink>
-            </div>
-            <NavLink
-              to="/contactus"
-              onClick={() => {
-                setOpenLogin(true);
-              }}
-              className={
-                "flex border-b-[1px] border-slate-300 w-full items-center justify-center p-2"
-              }
-            >
-              Contact Us
-            </NavLink>
 
-            <NavLink
-              to="/privacy"
-              onClick={() => {
-                setOpenLogin(true);
-              }}
-              className={
-                "flex border-b-[1px] border-slate-300 w-full items-center justify-center p-2"
-              }
-            >
-              Privacy
-            </NavLink>
-            <NavLink
-              to="/toc"
-              onClick={() => {
-                setOpenLogin(true);
-              }}
-            >
-              Terms and conditions
-            </NavLink>
-          </div>
+      <div className={`hidden overflow-hidden md:block`} >
+        <div className={`flex flex-col overflow-hidden right-0 ease-in-out duration-300  w-3/4  gap-4 h-screen absolute bg-white z-40 text-slate-500 p-5 ${!isCollapsed ? " translate-x-0" : " translate-x-full"}`}  >
+          <NavLink
+            to="/signin"
+            onClick={() => {
+              setOpenLogin(true);
+            }}
+            className={
+              "flex border-b-[1px] border-slate-300 w-full items-center justify-center p-2"
+            }
+          >
+            SignIn
+          </NavLink>
+          <NavLink
+            to="/signup"
+            onClick={() => {
+              setOpenLogin(true);
+            }}
+            className={
+              "flex border-b-[1px] border-slate-300 w-full items-center justify-center p-2"
+            }
+          >
+            SignUp
+          </NavLink>
+
+          <NavLink
+            to="/contactus"
+            onClick={() => {
+              setOpenLogin(true);
+            }}
+            className={
+              "flex border-b-[1px] border-slate-300 w-full items-center justify-center p-2"
+            }
+          >
+            Contact Us
+          </NavLink>
+
+          <NavLink
+            to="/privacy"
+            onClick={() => {
+              setOpenLogin(true);
+            }}
+            className={
+              "flex border-b-[1px] border-slate-300 w-full items-center justify-center p-2"
+            }
+          >
+            Privacy
+          </NavLink>
+          <NavLink
+            to="/toc"
+            onClick={() => {
+              setOpenLogin(true);
+            }}
+            className={"flex w-full items-center justify-center p-2"}
+          >
+            Terms and conditions
+          </NavLink>
         </div>
-      )}
+      </div>
     </>
   );
 };
